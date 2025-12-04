74c5219bf999052487b6a2af2b848eebf16dd487
Wojciech Guziewski <wojciechguziewski@gmail.com>
2025-12-05 00:40:24 +0100

    Add parking view spot grid
    Co-authored-by: Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
    Co-authored-by: fmich7 <michalak.filip.04@gmail.com>

--- frontend/src/pages/Parking.tsx
+++ frontend/src/pages/Parking.tsx
@@ -5,52 +5,59 @@ import { ParkingGrid } from "../components/reservation/Grid";
 import { ErrorBanned } from "../components/Banned";
 
 export function ParkingPage() {
-  const [data, setData] = useState<SpotState[]>([]);
-  const [isBanned, setIsBanned] = useState(true);
-  const [showParking, setShowParking] = useState(true);
-  const [banDays, setBanDays] = useState(3);
-  const [banReason, setBanReason] = useState("przekroczenie limitu rezerwacji");
-  const [chargeAmount, setChargeAmount] = useState(150);
-  const defaultStartDate = new Date();
-  const defaultEndDate = new Date();
+    const [data, setData] = useState<SpotState[]>([]);
+    const [isBanned, setIsBanned] = useState(true);
+    const [showParking, setShowParking] = useState(true);
+    const [banDays, setBanDays] = useState(3);
+    const [banReason, setBanReason] = useState("przekroczenie limitu rezerwacji");
+    const [chargeAmount, setChargeAmount] = useState(150);
 
-  useEffect(() => {
-    const callApi = async () => {
-      try {
-        const data = await getSpots(defaultStartDate, defaultEndDate);
-        setData(data);
-      } catch {
-        console.log("Failed to fetch data");
-      }
-    };
-    callApi();
-  }, []);
+    // selected spot id
+    const [selectedId, setSelectedId] = useState<number | null>(null);
 
-  useEffect(() => {
-    if (isBanned) setShowParking(false);
-  }, [isBanned]);
+    useEffect(() => {
+        const defaultStartDate = new Date();
+        const defaultEndDate = new Date();
 
-  return (
-    <div className="parking-page">
-      {showParking ? (
-        <>
-          <h1>Parking View</h1> <ParkingGrid spots={data} />{" "}
-        </>
-      ) : (
-        <ErrorBanned
-          days={banDays}
-          reason={banReason}
-          charge={chargeAmount}
-          onPay={() => {
-            alert("Blokada została opłacona");
-            setIsBanned(false);
-            setShowParking(true);
-          }}
-          onWait={() => {
-            setShowParking(true);
-          }}
-        />
-      )}
-    </div>
-  );
+        const callApi = async () => {
+            try {
+                const retrieved = await getSpots(defaultStartDate, defaultEndDate);
+                setData(retrieved);
+            } catch {
+                console.log("Failed to fetch data");
+            }
+        };
+
+        void callApi();
+    }, []);
+
+    useEffect(() => {
+        if (isBanned) setShowParking(false);
+    }, [isBanned]);
+
+    return (
+        <div className="parking-page">
+            {showParking ? (
+                <ParkingGrid
+                    spots={data}
+                    selectedId={selectedId}
+                    setSelectedId={setSelectedId}
+                />
+            ) : (
+                <ErrorBanned
+                    days={banDays}
+                    reason={banReason}
+                    charge={chargeAmount}
+                    onPay={() => {
+                        alert("Blokada została opłacona");
+                        setIsBanned(false);
+                        setShowParking(true);
+                    }}
+                    onWait={() => {
+                        setShowParking(true);
+                    }}
+                />
+            )}
+        </div>
+    );
 }
