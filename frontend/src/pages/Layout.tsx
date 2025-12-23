4d7c31d825f94ab38de5de86506e607664f2b95c
worender <wgmojastrona@gmail.com>
2025-12-23 21:12:54 +0100

    Improvement labels and css (like add hover to links).
    Add new link/button 'logout'.

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -12,26 +12,28 @@ export function Layout(){
 
     const mobileStyles = `
     
-    .p-tabmenu .p-tabmenu-nav .p-tabmenuitem .p-menuitem-link, .p-button-logout {
+    .p-tabmenu .p-tabmenu-nav .p-tabmenuitem .p-menuitem-link {
         border-bottom: none;
         background-color: lightgrey;
         border-radius: 20px ;
-       
-       
     }
     
+     .p-tabmenu .p-tabmenu-nav .p-tabmenuitem .p-menuitem-link:hover {
+        background-color: #ccccff;
+        time-transition: background-color 0.5s ease;
+        }
+        
     .p-tabmenu .p-tabmenu-nav .p-tabmenuitem.p-highlight .p-menuitem-link{
         background-color: #9999ff;
         time-transition: background-color 0.5s ease;
+        color: black;
        
     }
     
     
-    .p-button-logout{
-        padding: 20px;
-        }
+    
        
-         .p-tabmenu .p-tabmenu-nav, .p-button-logout {
+         .p-tabmenu .p-tabmenu-nav {
         display: flex;
         flex-wrap: wrap;
         gap: 5px;
@@ -49,35 +51,46 @@ export function Layout(){
         margin-bottom:2rem;
         }
         
+        #logout .p-menuitem-link{
+        background-color: #ff6666;
+        color: white;
+        }
+        
+        #logout .p-menuitem-icon{
+        margin-right: 0;
+        }
+        
+        #logout .p-menuitem-link:hover{
+        background-color: #ff3333;
+        
+        }
     `;
 
     let navigate = useNavigate();
 
-    // const logout = () => {
-    //     localStorage.removeItem('authToken');
-    //     navigate('/login');
-    // }
+    const logout = () => {
+        localStorage.removeItem('authToken');
+        navigate('/Login');
+    }
 
     const [activeIndex, setActiveIndex] = useState(() => {
         const path = window.location.pathname;
         if (path.includes('History')) return 1;
         if (path.includes('Report')) return 2;
-        if (path.includes('Settings')) return 3;
+        if (path.includes('Account')) return 3;
         return 0;
     });
 
 
     const items = [
+        { label: '', icon: 'pi pi-sign-out', command: () => { logout()}, id: 'logout' },
         { label: 'Parking', icon: 'pi pi-car' , command: () => { navigate('/Parking'); setActiveIndex(0)}, },
         { label: 'Historia', icon: 'pi pi-history', command: () => { navigate('/History');setActiveIndex(1) } },
         { label: 'Zgłoszenia', icon: 'pi pi-ban', command: () => { navigate('/Report'); setActiveIndex(2)} },
-        { label: 'Ustawienia',  icon: 'pi pi-user', command: () => { navigate('/Settings');setActiveIndex(3) } },
-        // {
-        //     template: () => (
-        //         <Button icon="pi pi-sign-out" className="p-button-logout" onClick={logout}/>
-        //     )
-        //
-        // },
+        { label: 'Konto',  icon: 'pi pi-user', command: () => { navigate('/Account');setActiveIndex(3) } },
+        // { label: 'Admin',  icon: 'pi pi-cog', command: () => { navigate('/Admin');setActiveIndex(3) } },
+
+
     ];
 
     return(
