bfa35ba02158cafa12f364c8f0807714bd1c59b0
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2025-12-28 13:46:13 +0100

    Restore `App.tsx`

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,87 +1,37 @@
 import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
 import '../App.css';
 
 import { Button } from "primereact/button";
-import { formatDateWeek, formatTime } from "../utils/dateUtils.ts";
-import { Divider } from "primereact/divider";
-import { Message } from "primereact/message";
-import { DateTimeDialog } from "../components/DateTimeDialog";
-import { DateTimeSelection } from "../components/DateTimeDialog/DateTimeSelection.ts";
 
 export function App() {
-    const [simpleVisible, setSimpleVisible] = useState(false);
-    const [rangeVisible, setRangeVisible] = useState(false);
-    const [singleVisible, setSingleVisible] = useState(false);
+    const [count, setCount] = useState(0);
 
-    const [externalState, setExternalState] = useState(DateTimeSelection.initial("single"));
-
-    const [msg, setMsg] = useState("");
-
-    const toMsg = ({ days, startTime, endTime }: DateTimeSelection<"single">) =>
-        `${formatDateWeek(days)}, ${formatTime(startTime!)} - ${formatTime(endTime!)}`;
+    function increment() {
+        setCount(count + 1);
+    }
 
     return (
         <>
-            <Message text={msg}/>
-
-            <Divider/>
-
-            <Message text={toMsg(externalState)}/>
-
-            <Divider/>
-
-            <Button onClick={() => setSimpleVisible(true)}>Simple single</Button>
-            <Button onClick={() => setRangeVisible(true)}>Range autoclose</Button>
-            <Button onClick={() => setSingleVisible(true)}>Single autoclose</Button>
-
-            <DateTimeDialog
-                header={"Wybierz datę i czas"}
-                visible={simpleVisible}
-                selectionMode="single"
-                initialSelection={externalState}
-                onApply={ selection => {
-                    setExternalState(selection);
-                    setSimpleVisible(false);
-                }}
-                onHide={() => setSimpleVisible(false)}
-            />
-
-            <DateTimeDialog
-                header="Wybierz zakres dat"
-                selectionMode="range"
-                visible={rangeVisible}
-                onClear={() => console.log("clear")}
-                isValid={ xs => (xs !== null && xs.days.length === 2 && xs.days[0] !== null && xs.days[1] !== null) }
-                onSelectionChange={ console.log }
-                showTimeSelect={false}
-                autoCloseOnDaySelect={true}
-                onHide={() => setRangeVisible(false)}
-                onApply={({ days, startTime, endTime }) => {
-                    const startMsg = startTime === null ? "null" : formatTime(startTime);
-                    const endMsg = endTime === null ? "null" : formatTime(endTime);
-                    const startDayMsg = days[0] === null ? "null" : formatDateWeek(days[0]);
-                    const endDayMsg = days[0] === null ? "null" : formatDateWeek(days[0]);
-
-                    setMsg(`Przedział od ${startDayMsg} do ${endDayMsg}, ${startMsg} - ${endMsg}`);
-
-                    setRangeVisible(false);
-                }}
-            />
-
-            <DateTimeDialog
-                header="Wybierz datę i przedzial godzin"
-                selectionMode="single"
-                autoCloseOnDaySelect={true}
-                visible={singleVisible}
-                onClear={() => console.log("clear")}
-                isValid={ x => !(x === null || x.days.getTime() < (new Date()).getTime()) }
-                onHide={() => setSingleVisible(false)}
-                onApply={({ days, startTime, endTime }) => {
-                    if (startTime !== null && endTime !== null)
-                        setMsg(`W dniu ${formatDateWeek(days)}, ${formatTime(startTime)} - ${formatTime(endTime)}`);
-                    setSingleVisible(false);
-                }}
-            />
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <Button onClick={increment} label={`count is ${count}`}/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
         </>
     );
 }
