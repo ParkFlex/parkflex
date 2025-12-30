fc1764b55a3cdd9c9edf6d0c3898803fd4945875
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2025-12-30 11:24:37 +0100

    Move DateTimeSelector isValid lambda to a function

--- frontend/src/components/reservation/DateTimeSelector.tsx
+++ frontend/src/components/reservation/DateTimeSelector.tsx
@@ -4,6 +4,7 @@ import { Chips } from "primereact/chips";
 import { Chip } from "primereact/chip";
 import { compareTime, formatDate, formatDateWeek, formatTime, isSameDay } from "../../utils/dateUtils.ts";
 import { Toolbar } from "primereact/toolbar";
+import type { DateTimeSelection } from "../DateTimeDialog/DateTimeSelection.ts";
 
 interface DateSelectionProps {
     visible: boolean;
@@ -55,6 +56,29 @@ export function DateTimeSelector(
                 icon="pi pi-pencil"
             />
         </div>;
+    
+    const validator = (day: Date, startTime: Date | null, endTime: Date | null) => {
+        const now = new Date();
+
+        if (startTime == null || endTime == null) return false;
+
+        const timeSpanSwapped = compareTime(startTime, endTime) >= 0;
+        if (timeSpanSwapped) return false;
+
+        const isToday = isSameDay(now, day);
+        if (isToday && compareTime(startTime, now) < 0) return false;
+
+        now.setHours(0);
+        now.setMinutes(0);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
+        const inPast = day.valueOf() < now.valueOf();
+        if (inPast) return false;
+
+        // TODO: max reservation time? padding, etc?
+
+        return true;
+    };
 
     return (
         <>
@@ -81,28 +105,7 @@ export function DateTimeSelector(
                     setTime([startTime!, endTime!]); // we assert the non-nullability in `isValid`
                 }}
 
-                isValid={({ days, startTime, endTime }) => {
-                    const now = new Date();
-
-                    if (startTime == null || endTime == null) return false;
-
-                    const timeSpanSwapped = compareTime(startTime, endTime) >= 0;
-                    if (timeSpanSwapped) return false;
-
-                    const isToday = isSameDay(now, days);
-                    if (isToday && compareTime(startTime, now) < 0) return false;
-
-                    now.setHours(0);
-                    now.setMinutes(0);
-                    now.setSeconds(0);
-                    now.setMilliseconds(0);
-                    const inPast = days.valueOf() < now.valueOf();
-                    if (inPast) return false;
-
-                    // TODO: max reservation time? padding, etc?
-
-                    return true;
-                }}
+                isValid={({ days, startTime, endTime }) => validator(days, startTime, endTime) }
             />
 
             {controls}
