595fda79fbb45065611392dbb054ef84851a6b94
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-01-01 16:01:55 +0100

    Add `type` col to the `parameter` table
    Enum at the language level. Varchar in the DB.

--- src/main/kotlin/db/parameter.kt
+++ src/main/kotlin/db/parameter.kt
@@ -3,13 +3,19 @@ package parkflex.db
 import org.jetbrains.exposed.dao.*
 import org.jetbrains.exposed.dao.id.*
 
+enum class ParameterType {
+    Number, String
+}
+
 object ParameterTable : LongIdTable("parameter") {
     val key = text("key")
+    val type = enumerationByName<ParameterType>("type", length = 6)
     val value = text("value")
 }
 
 class ParameterEntity(id: EntityID<Long>) : LongEntity(id) {
     var key by ParameterTable.key
+    var type by ParameterTable.type
     var value by ParameterTable.value
 
     companion object : LongEntityClass<ParameterEntity>(ParameterTable)
