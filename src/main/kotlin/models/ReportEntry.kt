7699311cdbbc39a0a4c1169145bd550a32304e55
miernior <dominik.miernik10@gmail.com>
2026-01-08 18:09:18 +0100

    created reportsRoutes.kt

--- src/main/kotlin/models/ReportEntry.kt
+++ src/main/kotlin/models/ReportEntry.kt
@@ -1,15 +1,14 @@
 package parkflex.models
-
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Contextual
 import java.time.LocalDateTime
 
 @Serializable
-data class ReportEntity (
+data class ReportEntry(
     val plate : String,
     @Contextual
     val issueTime : LocalDateTime?,
     val comment : String,
     val whoReported : String,
     val photo: String?
-    )
+)
--- src/main/kotlin/routes/reportsRoutes.kt
+++ src/main/kotlin/routes/reportsRoutes.kt
@@ -1,11 +1,34 @@
 package parkflex.routes
 
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
+import parkflex.db.ReportEntity
+import parkflex.models.ReportEntry
+import parkflex.runDB
 
 fun Route.reportsRoutes(){
     get{
+        var reportList : List<ReportEntry> = listOf()
+        runDB {
+            for (report in ReportEntity.all()) {
+            var penalty = report.penalty
+            var reservation = penalty?.reservation
+            var user = reservation?.user
+            val plate = user?.plate ?:"UNKNOWN"
+            var Entry = ReportEntry(plate,
+                report.timestamp,
+                report.description,
+                report.submitter.plate,
+                report.image
+                )
+                reportList += Entry
+            }
+        }
+        val sortedReports = reportList.sortedByDescending { it.issueTime }
 
+        call.respond(sortedReports)
     }
 }
