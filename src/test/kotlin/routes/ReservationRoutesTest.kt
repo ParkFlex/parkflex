7f464a044d7a834680a0e063116eb4db1414b44b
Maciej Lemański <maciej1lemanski@gmail.com>
2026-01-10 22:55:02 +0100

    test: add more tests

--- src/test/kotlin/routes/ReservationRoutesTest.kt
+++ src/test/kotlin/routes/ReservationRoutesTest.kt
@@ -18,6 +18,7 @@ import parkflex.db.SpotEntity
 import parkflex.db.SpotTable
 import parkflex.db.UserEntity
 import parkflex.db.UserTable
+import parkflex.models.ApiErrorModel
 import parkflex.models.CreateReservationRequest
 import parkflex.models.CreateReservationSuccessResponse
 import parkflex.runDB
@@ -71,8 +72,9 @@ class ReservationRoutesTest {
         val responseBody = response.body<CreateReservationSuccessResponse>()
         assertEquals("Rezerwacja utworzona pomyslnie", responseBody.message)
     }
+
     @Test
-    fun `test post reservation SAD user does't exist `() = testApplication{
+    fun `test post reservation SAD user doest exist `() = testApplication{
         val db = setupTestDB()
         application{ configureTest(db) }
         val client = testingClient()
@@ -104,7 +106,99 @@ class ReservationRoutesTest {
 
         assertEquals(HttpStatusCode.Unauthorized, response.status)
 
+        val responseBody = response.body<ApiErrorModel>()
+        assertEquals("Brak tokena lub token nieprawidlowy", responseBody.message)
+
     }
 
+    @Test
+    fun `test post reservation (spot = NULL) Take break go home `() = testApplication{
+        val db = setupTestDB()
+        application{ configureTest(db) }
+        val client = testingClient()
 
+        val (userId, spotId) = runDB {
+            SchemaUtils.create(UserTable, SpotTable, ReservationTable, PenaltyTable, ParameterTable)
+
+            val user = UserEntity.new(id = 2) {
+                fullName = "Integrator Test"
+                mail = "flow@parkflex.pl"
+                hash = "hash"
+                plate = "PO-FLOW1"
+                role = "user"
+            }
+            val spot = SpotEntity.new {
+                role = "abnormal"
+                displayOrder = 1
+            }
+            user.id.value to spot.id.value
+        }
+
+        val startTime = LocalDateTime.now().plusDays(1)
+        val startTimeString = startTime.format(DateTimeFormatter.ISO_DATE_TIME)
+        val reservationRequest = CreateReservationRequest(
+            spot_id = -100,
+            start = startTimeString,
+            duration = 45
+        )
+
+        val response = client.post("api/reservation"){
+            contentType(ContentType.Application.Json)
+            setBody(reservationRequest)
+        }
+
+        assertEquals(HttpStatusCode.NotFound, response.status)
+
+        val responseBody = response.body<ApiErrorModel>()
+        assertEquals("Miejsce parkingowe nie istnieje", responseBody.message)
+
+    }
+    // check code
+    @Test
+    fun `test post reservation (spot = !normal)`() = testApplication {
+        val db = setupTestDB()
+        application { configureTest(db) }
+        val client = testingClient()
+
+        val (userId, spotId) = runDB {
+            SchemaUtils.create(UserTable, SpotTable, ReservationTable, PenaltyTable, ParameterTable)
+
+            val user = UserEntity.new(id = 2) {
+                fullName = "Integrator Test"
+                mail = "flow@parkflex.pl"
+                hash = "hash"
+                plate = "PO-FLOW1"
+                role = "user"
+            }
+            val spot = SpotEntity.new {
+                role = "abnormal" // <-- here
+                displayOrder = 1
+            }
+            user.id.value to spot.id.value
+        }
+
+        val startTime = LocalDateTime.now().plusDays(1)
+        val reservationRequest = CreateReservationRequest(
+            spot_id = spotId,
+            start = startTime.format(DateTimeFormatter.ISO_DATE_TIME),
+            duration = 45
+        )
+
+        val response = client.post("api/reservation") {
+            contentType(ContentType.Application.Json)
+            setBody(reservationRequest)
+        }
+
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+
+        val responseBody = response.body<ApiErrorModel>()
+        assertEquals("Rezerwacje można tworzyć tylko na miejsce typu 'normal' (otrzymano abnormal)", responseBody.message)
+    }
+    //add more tests
+    // 1."Miejsce zajete w tym czasie"
+    // 2."Nieprawidlowy format daty"
+    // 3."Użytkownik ma już aktywną rezerwację ($day: $startTime-$endTime)"
+    // 4."Banowany uzytkownik nie moze robic rezerwacji"
+    // 5."Brak tokena lub token nieprawidlowy" użytkownik nie istnieje dodac jeszcze brak tokena autoryzacyjnego
+    // 6."Nieprawidlowe dane"
 }
