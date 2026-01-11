c96d60af4d84b30b8e1f9a1fdd9381f5870ef303
miernior <dominik.miernik10@gmail.com>
2026-01-11 15:44:09 +0100

    created AdminReportEntry.kt and reportsRoutes.kt

--- src/main/kotlin/routes/reportsRoutes.kt
+++ src/main/kotlin/routes/reportsRoutes.kt
@@ -6,28 +6,46 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import parkflex.db.ReportEntity
+import parkflex.models.AdminReportEntry
+import parkflex.models.PenaltyModel
 import parkflex.models.ReportEntry
 import parkflex.runDB
 
 fun Route.reportsRoutes(){
     get{
-        var reportList : List<ReportEntry> = listOf()
+        var AdminreportList : List<AdminReportEntry> = listOf()
         runDB {
             for (report in ReportEntity.all()) {
             var penalty = report.penalty
             var reservation = penalty?.reservation
             var user = reservation?.user
             val plate = user?.plate ?:"UNKNOWN"
-            var Entry = ReportEntry(plate,
-                report.timestamp,
-                report.description,
-                report.submitter.plate,
-                report.image
+            var Penalty = report.penalty
+
+                if( report.penalty != null){
+                penalty = report.penalty
+
+            }else
+            {
+                penalty = null
+            }
+            var Entry = AdminReportEntry(
+                id = report.id.value,
+                plate = plate,
+                timestamp = report.timestamp,
+                description = report.description,
+                submitterPlate = report.submitter.plate,
+                image = report.image,
+                reviewed = report.reviewed,
+                penalty = penalty
+
+
+
                 )
-                reportList += Entry
+                AdminreportList += Entry
             }
         }
-        val sortedReports = reportList.sortedByDescending { it.issueTime }
+        val sortedReports = AdminreportList.sortedByDescending { it.timestamp }
 
         call.respond(sortedReports)
     }
