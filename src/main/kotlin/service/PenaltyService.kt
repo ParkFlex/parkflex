d5d19ce5c7fcb0f59a6431ad6bbba30ecc50bb64
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-01-12 12:58:14 +0100

    Take penalty clashes into account

--- src/main/kotlin/service/PenaltyService.kt
+++ src/main/kotlin/service/PenaltyService.kt
@@ -7,8 +7,66 @@ import parkflex.repository.ParameterRepository
 import parkflex.runDB
 import java.time.Duration
 import java.time.LocalDateTime
+import kotlin.Long
 
 object PenaltyService {
+
+    /**
+     * Represents a penalty that has not yet been commited to the store.
+     */
+    private data class PartialPenalty(
+        val fine: Long,
+        val due: LocalDateTime,
+        val reason: PenaltyReason,
+        val paid: Boolean,
+    ) {
+        private val outer = this
+
+        /**
+         * Overwrites an existing penalty with this partial penalty.
+         *
+         * @return Updated penalty
+         */
+        fun overwrite(existingPenalty: PenaltyEntity): PenaltyEntity {
+            existingPenalty.fine = fine
+            existingPenalty.due = due
+            existingPenalty.reason = reason
+            existingPenalty.paid = paid
+
+            return existingPenalty
+        }
+
+        /**
+         * Commits a partial penalty into the database.
+         *
+         * @param reservation Reservation connected to the penalty
+         *
+         * @return Updated penalty
+         */
+        fun commit(reservation: ReservationEntity) = PenaltyEntity.new {
+            this.reservation = reservation
+            this.reason = outer.reason
+            this.fine = outer.fine
+            this.due = outer.due
+            this.paid = outer.paid
+        }
+    }
+
+    /**
+     * Resolves a clash when a new penalty is being created for a reservation
+     * which already has an existing penalty.
+     *
+     * The strategy is that we pick the penalty with higher fine.
+     *
+     * @param existingPenalty Penalty found in the store
+     * @param partialPenalty Penalty in construction
+     *
+     * @return Updated penalty
+     */
+    private fun resolveClash(existingPenalty: PenaltyEntity, partialPenalty: PartialPenalty): PenaltyEntity =
+        if (existingPenalty.fine < partialPenalty.fine) partialPenalty.overwrite(existingPenalty)
+        else existingPenalty
+
     /***
      * Creates a penalty if an overtime is detected.
      *
@@ -22,23 +80,29 @@ object PenaltyService {
 
         val overtimeMinutes = Duration.between(reservation.end().plusMinutes(breakTime), reservation.left).toMinutes()
 
-        if (overtimeMinutes > 0) {
-            val per15min = ParameterRepository.get("penalty/fine/overtime")?.toLong()!!
-            val fine = (overtimeMinutes / 15) * per15min
+        when {
+            overtimeMinutes > 0 -> {
+                val per15min = ParameterRepository.get("penalty/fine/overtime")?.toLong()!!
+                val fine = (overtimeMinutes / 15) * per15min
 
-            val penaltyHours = ParameterRepository.get("penalty/block/duration")?.toLong()!!
+                val penaltyHours = ParameterRepository.get("penalty/block/duration")?.toLong()!!
 
-            val due = timestamp.plusHours(penaltyHours)
+                val due = timestamp.plusHours(penaltyHours)
 
-            PenaltyEntity.new {
-                this.reservation = reservation
-                this.reason = PenaltyReason.Overtime
-                this.fine = fine
-                this.due = due
-                this.paid = false
+                val partialPenalty = PartialPenalty(
+                    fine = fine,
+                    due = due,
+                    reason = PenaltyReason.Overtime,
+                    paid = false,
+                )
+
+                when (val existingPenalty: PenaltyEntity? = reservation.penalties.singleOrNull()) {
+                    null -> partialPenalty.commit(reservation)
+                    else -> resolveClash(existingPenalty, partialPenalty)
+                }
             }
-        }
 
-        null
+            else -> null
+        }
     }
 }
