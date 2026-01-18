23e4e1dc7cc1ca03cfd1c6b3663fbfc955fc9fde
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-01-18 14:18:59 +0100

    Add `usePrelude` hook

---
+++ frontend/src/hooks/usePrelude.ts
@@ -0,0 +1,43 @@
+import { type AxiosResponse } from "axios";
+import type {PenaltyInformation, PreludeModel} from "../models/PreludeModel.ts";
+import {useCallback, useEffect, useState} from "react";
+import { useAxios } from "./useAxios.ts";
+
+export const usePrelude = () => {
+    const axios = useAxios();
+    const [prelude, setPrelude] = useState<PreludeModel | null>(null);
+
+    const emptyPrelude: PreludeModel = {
+        minReservationTime: 30,
+        maxReservationTime: 720,
+        penaltyInformation: null
+    };
+
+    const onThen = (x: AxiosResponse) => {
+        const data = x.data;
+        const penaltyInformation: PenaltyInformation | null = data.penaltyInformation && ({
+            fine: Number(data.penaltyInformation.fine),
+            due: new Date(data.penaltyInformation.due),
+            reason: data.penaltyInformation.fine
+        });
+
+        const got: PreludeModel = {
+            minReservationTime: Number(data.minReservationTime),
+            maxReservationTime: Number(data.maxReservationTime),
+            penaltyInformation: penaltyInformation
+        };
+
+        setPrelude(got);
+    };
+
+    const getPrelude = useCallback(() => {
+        axios.get("/prelude").then(onThen);
+    }, [axios]);
+
+    useEffect(() => {
+        getPrelude();
+    }, []);
+
+
+    return { prelude: prelude || emptyPrelude, getPrelude };
+};
---
+++ frontend/src/models/PenaltyReason.ts
@@ -0,0 +1,1 @@
+export type PenaltyReason = "WrongSpot" | "Overtime" | "NotArrived";
