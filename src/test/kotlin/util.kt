e8ce66800dc36c68238265b9df6060cdc3af53ce
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-01-20 00:51:58 +0100

    Add `ArrivalRoutesTests`

--- src/test/kotlin/util.kt
+++ src/test/kotlin/util.kt
@@ -3,6 +3,7 @@ import io.ktor.client.plugins.sse.sse
 import kotlinx.coroutines.flow.first
 import parkflex.db.UserEntity
 import parkflex.repository.JwtRepository
+import java.util.UUID
 
 fun dummyToken(uid: Long) =
     JwtRepository.generateToken(uid, "dummy@parkflex.pl", "user")
@@ -17,9 +18,17 @@ fun newAdmmin() = UserEntity.new(1) {
     this.hash = ""
 }
 
+fun newUser() = UserEntity.new {
+    this.fullName = "user"
+    this.mail = "user" + UUID.randomUUID().toString()
+    this.plate = ""
+    this.role = "user"
+    this.hash = ""
+}
+
 suspend fun HttpClient.firstSSE(path: String): String? {
     var token: String? = null
-    sse("/term/exit") {
+    sse(path) {
         incoming.first().data?.let { token = it }
     }
 
